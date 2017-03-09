e8bc437cd4d3677a45746d4a041b5f09446eca82
uynk <uybkt2@gmail.com>
2017-03-10 00:53:59 +0700

    Update

--- socket_client.js
+++ socket_client.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
-	var socket = io.connect('http://192.168.1.7:8080/');
-	//var socket = io.connect('https://tienlen.herokuapp.com');
+	//var socket = io.connect('http://192.168.1.7:8080/');
+	var socket = io.connect('https://tienlen.herokuapp.com');
 	
 	socket.on('create_table', function(data){
 		//alert(data);
-		$('#hw1').text(data.player_id);
+		$('p').text(data.player_id);
 	})
 	
 	socket.on('server_sent', function(data){
 		//alert(data);
-		$('#hw2').text(data);
+		$('p').text(data);
 	})
 
 	$('#go').click(function(){
 		socket.emit('client_sent', $('#name').val());
-		socket.emit('create_table', "{\"player_id\":\"web\"}");
+		//socket.emit('create_table', "{\"player_id\":\"web\"}");
 	})
 })
 	
